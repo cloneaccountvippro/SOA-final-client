5f006cb83a322b2a75d23072c3b695ed36450499
@S52100903 <52100903@student.tdtu.edu.vn>
2024-04-28 19:16:29 +0700

    refactor: updating rooms booking service

--- src/pages/rooms/components/page.jsx
+++ src/pages/rooms/components/page.jsx
@@ -17,6 +17,7 @@ function RoomsPage() {
 
     const [selectedRooms, setSelectedRooms] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const [showAvailableRooms, setShowAvailableRooms] = useState(false);
     const itemsPerPage = 8;
 
     // Function to handle input change for customer data fields
@@ -33,7 +34,7 @@ function RoomsPage() {
         const phoneNumber = event.target.value;
         setCustomerData((prevCustomerData) => {
             const foundCustomer = customers.find((customer) => customer.phone_number === phoneNumber);
-            if (foundCustomer) {   
+            if (foundCustomer) {
                 // If a customer is found, update other fields with customer's data
                 const { phone_number, ...customerWithoutPhoneNumber } = foundCustomer; // Exclude phone number from update
                 return {
@@ -83,90 +84,101 @@ function RoomsPage() {
     // Calculate index of the first and last item to display on the current page
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = rooms.slice(indexOfFirstItem, indexOfLastItem);
+
+    // Filter rooms based on availability
+    const availableRooms = rooms.filter(room => room.isActive);
+
+    // Filter rooms based on pagination and availability
+    const currentItems = showAvailableRooms ? availableRooms.slice(indexOfFirstItem, indexOfLastItem) : rooms.slice(indexOfFirstItem, indexOfLastItem);
 
     // Check if all customer fields are filled
     const isCustomerDataComplete = Object.values(customerData).every(value => value !== "");
 
     return (
-        <div className="flex border-[1px] border-slate-300 rounded-sm w-fit p-5 space-x-7">
-            {/* Customer Section */}
-            <div>
-                <h2 className="font-semibold text-2xl">Customer Information</h2>
-                <form className="w-[400px] mt-5">
-                    {/* Input field for phone number */}
-                    <div className="flex items-center gap-2 justify-between">
-                        <label htmlFor="phone">Phone Number:</label>
-                        <Input
-                            type="tel"
-                            id="phone"
-                            name="phone_number"
-                            value={customerData.phone_number}
-                            onChange={handlePhoneNumberChange}
-                            className="w-[70%]"
-                            placeholder="Enter phone number to find customer"
-                        />
-                    </div>
-                    {/* Input fields for customer data */}
-                    {Object.keys(customerData).map((key) => {
-                        if (key !== "phone_number" && key !== 'id') { // Exclude phone number field from rendering
-                            return (
-                                <div key={key} className="flex items-center gap-2 my-2 justify-between">
-                                    <label htmlFor={key}>{key.replace('_', ' ').toUpperCase()}:</label>
-                                    <Input
-                                        type="text"
-                                        id={key}
-                                        name={key}
-                                        value={customerData[key]}
-                                        onChange={handleCustomerDataChange}
-                                        className="w-[70%]"
-                                    />
-                                </div>
-                            );
-                        } else {
-                            return null;
-                        }
-                    })}
-                </form>
-                {/* Button to submit customer data */}
-                <Button disabled={!isCustomerDataComplete} className="mt-3">Submit Customer Data</Button>
-            </div>
-            {/* Room Section */}
-            <div>
-                <h1 className="font-semibold text-2xl mb-5">Rooms</h1>
-                <div style={{ overflowY: 'auto', maxHeight: '500px' }}>
-                    <table className="border-collapse border border-gray-400 w-[400px]">
-                        <thead>
-                            <tr>
-                                <th className="border border-gray-400 p-2 w-[33%]">Name</th>
-                                <th className="border border-gray-400 p-2 w-[33%]">Type</th>
-                                <th className="border border-gray-400 p-2 w-[33%]">Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {currentItems.map((room) => (
-                                <tr
-                                    key={room.id}
-                                    className={room.isActive ? (selectedRooms.includes(room.id) ? "bg-blue-500 cursor-pointer" : "hover:bg-blue-300 cursor-pointer") : "bg-gray-200"}
-                                    onClick={() => handleRoomSelection(room.id)}
-                                >
-                                    <td className="border border-gray-400 p-2">{room.name}</td>
-                                    <td className="border border-gray-400 p-2">{room.type}</td>
-                                    <td className="border border-gray-400 p-2">{room.price}$</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+        <div className="w-full h-full flex flex-col items-center justify-center">
+            <div className="flex border-[1px] border-slate-300 rounded-sm w-fit p-5 space-x-7">
+                {/* Customer Section */}
+                <div>
+                    <h2 className="font-semibold text-2xl">Customer Information</h2>
+                    <form className="w-[400px] mt-5">
+                        {/* Input field for phone number */}
+                        <div className="flex items-center gap-2 justify-between">
+                            <label htmlFor="phone">Phone Number:</label>
+                            <Input
+                                type="tel"
+                                id="phone"
+                                name="phone_number"
+                                value={customerData.phone_number}
+                                onChange={handlePhoneNumberChange}
+                                className="w-[70%]"
+                                placeholder="Enter phone number to find customer"
+                            />
+                        </div>
+                        {/* Input fields for customer data */}
+                        {Object.keys(customerData).map((key) => {
+                            if (key !== "phone_number" && key !== 'id') { // Exclude phone number field from rendering
+                                return (
+                                    <div key={key} className="flex items-center gap-2 my-2 justify-between">
+                                        <label htmlFor={key}>{key.replace('_', ' ').toUpperCase()}:</label>
+                                        <Input
+                                            type="text"
+                                            id={key}
+                                            name={key}
+                                            value={customerData[key]}
+                                            onChange={handleCustomerDataChange}
+                                            className="w-[70%]"
+                                        />
+                                    </div>
+                                );
+                            } else {
+                                return null;
+                            }
+                        })}
+                    </form>
+                    {/* Button to submit customer data */}
+                    <Button disabled={!isCustomerDataComplete} className="mt-3">Booking Room</Button>
                 </div>
-                {/* Pagination */}
-                <div className="mt-5 flex justify-end">
-                    <div className="space-x-2">
-                        {Array.from({ length: totalPages }, (_, i) => (
-                            <Button key={i} onClick={() => handlePageChange(i + 1)}>
-                                {i + 1}
-                            </Button>
-                        ))}
+                {/* Room Section */}
+                <div>
+                    <h1 className="font-semibold text-2xl mb-5">Rooms</h1>
+                    <div style={{ overflowY: 'auto', maxHeight: '500px' }}>
+                        <table className="border-collapse border border-gray-400 w-[400px]">
+                            <thead>
+                                <tr>
+                                    <th className="border border-gray-400 p-2 w-[33%]">Name</th>
+                                    <th className="border border-gray-400 p-2 w-[33%]">Type</th>
+                                    <th className="border border-gray-400 p-2 w-[33%]">Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {currentItems.map((room) => (
+                                    <tr
+                                        key={room.id}
+                                        className={room.isActive ? (selectedRooms.includes(room.id) ? "bg-blue-500 cursor-pointer" : "hover:bg-blue-300 cursor-pointer") : "bg-gray-200"}
+                                        onClick={() => handleRoomSelection(room.id)}
+                                    >
+                                        <td className="border border-gray-400 p-2">{room.name}</td>
+                                        <td className="border border-gray-400 p-2">{room.type}</td>
+                                        <td className="border border-gray-400 p-2">{room.price}$</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
+                    {/* Pagination */}
+                    <div className="mt-5  flex justify-end">
+                        <div className="space-x-2">
+                            {Array.from({ length: totalPages }, (_, i) => (
+                                <Button key={i} onClick={() => handlePageChange(i + 1)} className="w-2 h-[30px]">
+                                    {i + 1}
+                                </Button>
+                            ))}
+                        </div>
+                    </div>
+                    {/* Button to toggle available rooms */}
+                    <Button onClick={() => setShowAvailableRooms(!showAvailableRooms)} className="">
+                        {showAvailableRooms ? "Show All Rooms" : "Show Available Rooms Only"}
+                    </Button>
                 </div>
             </div>
         </div>
---
+++ src/pages/service/test/data/service.ts
@@ -0,0 +1,12 @@
+export const services = [
+    {id: 1, name: "Shuttle Bus", price: 100},
+    {id: 2, name: "Taxi", price: 50},
+    {id: 3, name: "Room Cleaning", price: 50},
+    {id: 4, name: "Food", price: 200},
+    {id: 5, name: "Drink", price: 100},
+    {id: 6, name: "Dining", price: 300},
+    {id: 7, name: "Breakfast", price: 100},
+    {id: 8, name: "Lunch", price: 200},
+    {id: 9, name: "Golf", price: 200},
+    {id: 10, name: "Gym", price: 700},
+]
